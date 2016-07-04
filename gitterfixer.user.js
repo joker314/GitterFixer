0f363aea80ce19b6e7a68754eab101af43593b7a
joker314 <maxzuba22@yahoo.co.uk>
2016-07-04 19:18:23 +0100

    Delete gitterfixer.user.js
    Delete gitterfixer.user.js

--- gitterfixer.user.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name        Gitter Fixer
-// @namespace   https://scratch.mit.edu/users/Tymewalk
-// @description A userscript to remove the green notice you get when you recieve a message on Gitter
-// @include     http://gitter.im/*
-// @include     https://gitter.im/*
-// ==/UserScript==
