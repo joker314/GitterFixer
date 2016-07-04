295d1df40d577fd94cb932d3f13a54c52a693edd
joker314 <maxzuba22@yahoo.co.uk>
2016-07-04 20:00:21 +0100

    Update gitter.user.js

--- gitter.user.js
+++ gitter.user.js
@@ -5,3 +5,6 @@
 // @include     http://gitter.im/*
 // @include     https://gitter.im/*
 // ==/UserScript==
+var bodge = document.createElement("STYLE");
+bodge.appendChild(document.createTextNode(".notification-text{visibility:hidden; display:none;}"));
+document.body.appendChild(bodge);
